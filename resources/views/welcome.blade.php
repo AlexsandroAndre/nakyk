6155b2af5c7ac5548f07878697d917bb9d26414c
Alexsandro Andre <miojoalex@gmail.com>
2018-11-13 00:43:22 -0200

    teste
    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,11 +11,11 @@ oi
 
     <script type="text/javascript">
         // ESDK page and bar title
-        window.mainPageTitle = 'Welcome Page';
-            ShopifyApp.ready(function() {
-                ShopifyApp.Bar.initialize({
-                    title: 'Welcome'
-                })
-            });
+        // window.mainPageTitle = 'Welcome Page';
+        //     ShopifyApp.ready(function() {
+        //         ShopifyApp.Bar.initialize({
+        //             title: 'Welcome'
+        //         })
+        //     });
     </script>
 @endsection
