2a027b0d7694f17f37e098d40a94242278df98c3
Alexsandro Andre <miojoalex@gmail.com>
2018-11-19 20:46:08 -0200

    teste

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,6 @@
 @extends('shopify-app::layouts.default')
 
 @section('content')
-<?php echo phpinfo(); ?>
     <link rel="stylesheet" href="https://sdks.shopifycdn.com/polaris/3.0.0/polaris.min.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     
