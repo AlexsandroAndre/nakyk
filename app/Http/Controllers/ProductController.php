e5ff850b66ce5c715fdb4d9ad703a54293ca8602
Alexsandro Andre <miojoalex@gmail.com>
2018-11-20 01:06:54 -0200

    teste

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -151,7 +151,7 @@ class ProductController extends Controller
     {
         $produtos_barra = array();
         $query = $this->query_builder("SELECT * FROM produtos_barra WHERE produto =" . $p->PRODUTO);
-        array_push($produtos_barra, array_map(array($this, 'produto_cores'), $query));
+        array_push(get_object_vars($p->produtos_barra), array_map(array($this, 'produto_cores'), $query));
         $p->produtos_barra = $produtos_barra;
         return $p;
     }
