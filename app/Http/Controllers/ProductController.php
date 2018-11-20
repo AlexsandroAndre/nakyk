147820063fa9ea4c334feaa093c776389d07c5d9
Alexsandro Andre <miojoalex@gmail.com>
2018-11-20 00:18:13 -0200

    t

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -144,7 +144,7 @@ class ProductController extends Controller
         foreach($collection as $p)
         {
             echo '<pre>loop..';
-                var_dump($p); 
+                var_dump($p->PRODUTO); 
                 echo '</pre>';
             $p->produtos_barra = DB::connection('sqlsrv')->select("SELECT * FROM produtos_barra WHERE produto =" . $p->produto);  
             array_push($produto, $p);
